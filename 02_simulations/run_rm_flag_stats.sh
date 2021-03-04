ae1454ed96209606085fd2b207662d40f020ee36
npaulat <34585535+npaulat@users.noreply.github.com>
2021-03-04 10:11:12 -0600

    Create run_rm_flag_stats.sh

---
+++ 02_simulations/run_rm_flag_stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIRECTORY=$1
+PREFIX=$2
+
+WDIR="/lustre/scratch/npaulat/ESAT"
+cd $WDIR
+
+IDS=()
+while read ID; do IDS+=($ID); done < esat_batch_${PREFIX}_ids.txt
+
+cd $DIRECTORY
+
+for i in {0..299}; do cd ${IDS[$i]}; bash ../../rm_flag_stats.sh ${IDS[$i]}; cd ..; done
